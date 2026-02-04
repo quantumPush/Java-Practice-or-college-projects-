2224ecd5c76e6e658bf6a46ae208051072755d33
Rohini Bhalavi <rohinibhalavi718@gmail.com>
2026-02-04 18:14:22 +0530

    Add ArrayList example

---
+++ Collections/ArrayListExample.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Rohini");
+        names.add("Amit");
+        names.add("Sneha");
+
+        System.out.println(names);
+    }
+}
