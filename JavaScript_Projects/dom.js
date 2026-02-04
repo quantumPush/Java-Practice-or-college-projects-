64727c1f1b4c7ccb33974eeed3633ca510740656
Rohini Bhalavi <rohinibhalavi718@gmail.com>
2026-02-04 17:46:52 +0530

    Add DOM manipulation JavaScript

---
+++ JavaScript_Projects/dom.js
@@ -0,0 +1,7 @@
+function changeText() {
+    document.getElementById("text").innerHTML = "Text changed using DOM!";
+}
+
+function changeColor() {
+    document.getElementById("text").style.color = "blue";
+}
