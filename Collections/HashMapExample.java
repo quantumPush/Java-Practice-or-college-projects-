fa6db0da458fe29300fe5ab24e2ca50f558022a1
Rohini Bhalavi <rohinibhalavi718@gmail.com>
2026-02-04 18:15:16 +0530

    Add HashMap example

---
+++ Collections/HashMapExample.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(1, "Java");
+        map.put(2, "Python");
+        map.put(3, "HTML");
+
+        System.out.println(map);
+    }
+}
