fb793b49a4e8d0d844e3f6893c8a853dcbe0f9ad
Rohini Bhalavi <rohinibhalavi718@gmail.com>
2026-02-11 14:14:17 +0530

    AddStudentService class

--- MiniProjects/StudentManagementSystem/src/StudentService.java
+++ MiniProjects/StudentManagementSystem/src/StudentService.java
@@ -1,24 +1,24 @@
-packge service;
+package service;
+
 import model.Student;
 import java.util.ArrayList;
 
-public class StudntService{
+public class StudentService {
 
-private ArrayList<Student>studentList=new ArrayList<>();
+    private ArrayList<Student> studentList = new ArrayList<>();
 
-public void addStudent(Student
-student){
-  studentList.add(student);
-  System.out.println("Student Added Successfullu!");
-}
-  public void viewStudents(){
-    if(studentList.isEmpty()){
-      System.out.println("No students available.");
-    }else{
-      for(Student s:
-          studentList){
-        System.out.println(s);
-      }
+    public void addStudent(Student student) {
+        studentList.add(student);
+        System.out.println("Student Added Successfully!");
     }
-  }
+
+    public void viewStudents() {
+        if(studentList.isEmpty()) {
+            System.out.println("No students available.");
+        } else {
+            for(Student s : studentList) {
+                System.out.println(s);
+            }
+        }
+    }
 }
