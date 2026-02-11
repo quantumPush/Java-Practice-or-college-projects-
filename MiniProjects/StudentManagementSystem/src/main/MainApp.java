7edc65928b1e72fef998b85211f66695022532a7
Rohini Bhalavi <rohinibhalavi718@gmail.com>
2026-02-11 13:56:21 +0530

    Remove Main folder structure

--- MiniProjects/StudentManagementSystem/src/main/MainApp.java
+++ MiniProjects/StudentManagementSystem/src/main/MainApp.java
@@ -1,10 +1,1 @@
-package main;
 
-import controller.StudentController;
-
-public class MainApp {
-    public static void main(String[] args) {
-        StudentController controller = new StudentController();
-        controller.start();
-    }
-}
