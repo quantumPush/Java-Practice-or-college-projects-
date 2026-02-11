72239de3520cac0c66a7facfd62e1ec9df4f511f
Rohini Bhalavi <rohinibhalavi718@gmail.com>
2026-02-11 13:13:35 +0530

    Update StudentService with ArrayList

--- MiniProjects/StudentManagementSystem/src/model/src/service/StudentService.java
+++ MiniProjects/StudentManagementSystem/src/model/src/service/StudentService.java
@@ -1,11 +1,24 @@
 package service;
 
 import model.Student;
+import java.util.ArrayList;
 
 public class StudentService {
 
-    public void addStudent() {
-        Student s1 = new Student(1, "Rohini", 21);
-        s1.display();
+    ArrayList<Student> students = new ArrayList<>();
+
+    public void addStudent(Student s) {
+        students.add(s);
+        System.out.println("Student Added Successfully!");
+    }
+
+    public void viewStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students found.");
+        } else {
+            for (Student s : students) {
+                System.out.println(s);
+            }
+        }
     }
 }
